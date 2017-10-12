de2067d5f2ddbb207bed7ebed9adb674441d4e9c
sprek <sprekk@gmail.com>
2017-10-12 18:36:48 -0400

    added big size

---
+++ __init__.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, g, request, redirect, url_for, session, jsonify
+from flask_bootstrap import Bootstrap, StaticCDN
+from codename import codename_view
+import os, sqlite3, random, socket
+import time
+import logging
+import logging.handlers
+
+app = Flask(__name__)
+Bootstrap(app)
+app.extensions['bootstrap']['cdns']['jquery'] = StaticCDN()
+app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN()
+app.secret_key = b'FF\x90}\xdc\xc5\xaeaT\xd6\xbc\x86O\xa6B\xdd\xa2qp\x9e\xd2f\xe8\xe8'
+
+LOG_FILENAME='/home/sprek/logs/user/flask_webapp.log'
+
+cur_host = socket.gethostname()
+if 'webfaction' in cur_host:
+    file_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=5242880, backupCount=10)
+    app.logger.addHandler(file_handler)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# --------------------------------------------------                                                                                                                                                                 
+# codename                                                                                                                                                                                                           
+
+@app.route('/codename')
+def codename():
+    return render_template('codename/codename.html')
+
+@app.route('/codename/leader')
+def leader():
+    return render_template('codename/leader.html', code_html=codename_view.get_leader_html())
+
+@app.route('/codename/set_seed_leader', methods=('POST','GET'))
+def set_seed_leader():
+    seed=int(request.form["seed_input"].strip())
+    size=request.form["size_select"].strip()
+    return codename_view.get_leader_html(seed, size)
+
+@app.route('/codename/set_seed_game', methods=('POST','GET'))
+def set_seed_game():
+    seed = None
+    if request.form["seed_input"]:
+        seed=int(request.form["seed_input"].strip())
+    dict_select=request.form["dict_select"].strip()
+    size=request.form["size_select"].strip()
+    return codename_view.get_game_html(dict_select,seed,size)
+
+@app.route('/codename/play')
+def play():
+    return render_template('codename/play.html', code_html=codename_view.get_game_html("Dictionary", size='normal'))
+
+if __name__ == '__main__':
+    #app.add_url_rule('/favicon.ico',redirect_to=url_for('static', filename='favicon.ico'))                                                                                                                          
+    app.run(host='0.0.0.0', debug=True)
+
+
---
+++ codename/codename_view.py
@@ -0,0 +1,155 @@
+import random
+import socket
+from codename import codename_game_info as game_info
+
+
+HOME_CAH_FILE="/home/daniel/development/projects/sprek/codenames/cah_code_names.txt"
+HOME_POP_CULTURE_FILE="/home/daniel/development/projects/sprek/codenames/code_names_pop.txt"
+HOME_DICTIONARY_FILE="/home/daniel/development/projects/sprek/codenames/code_names_dict.txt"
+HOME_SIMPLE_FILE="/home/daniel/development/projects/sprek/codenames/code_names_simple.txt"
+SERVER_CAH_FILE="/home/sprek/cah_code_names.txt"
+SERVER_POP_CULTURE_FILE="/home/sprek/code_names_pop.txt"
+SERVER_DICTIONARY_FILE="/home/sprek/code_names_dict.txt"
+SERVER_SIMPLE_FILE="/home/sprek/code_names_simple.txt"
+
+FILE_LOOKUP_HOME={"CAH" : HOME_CAH_FILE, "Pop Culture" : HOME_POP_CULTURE_FILE, "Dictionary" : HOME_DICTIONARY_FILE, "Simple" : HOME_SIMPLE_FILE}
+FILE_LOOKUP_SERVER={"CAH" : SERVER_CAH_FILE, "Pop Culture" : SERVER_POP_CULTURE_FILE, "Dictionary" : SERVER_DICTIONARY_FILE, "Simple" : SERVER_SIMPLE_FILE}
+
+red_color = '#df2020'
+blue_color= '#207fdf'
+black_color= '#505050'
+white_color= '#cccccc'
+blackout_color= '#000000'
+
+SIZE_TYPE_NORMAL='normal'
+SIZE_TYPE_BIG='big'
+
+NUM_WORDS_NORMAL=25
+NUM_WORDS_BIG=81
+
+BIG_BLACKOUT_SPOTS=[4,20,24,36,40,44,56,60,76]
+
+def get_dict_path(file_lookup):
+    cur_host = socket.gethostname()
+    if 'webfaction' in cur_host:
+        return FILE_LOOKUP_SERVER[file_lookup]
+    return FILE_LOOKUP_HOME[file_lookup]
+
+def get_color(char):
+    if char == 'R':
+        return red_color
+    elif char == 'B':
+        return blue_color
+    elif char == 'X':
+        return black_color
+    elif char == '-':
+        return blackout_color
+    else:
+        return white_color
+
+def get_player_info(dictionary, num_words=NUM_WORDS_NORMAL):
+    if not dictionary in FILE_LOOKUP_SERVER.keys():
+        print ("Error: dictionary '" + dictionary + "' doesn't exist")
+        return None
+    words_file = open(get_dict_path(dictionary), 'r')
+    words = [elem for elem in words_file.read().split('\n') if len(elem.strip()) > 0]
+    random.shuffle(words)
+    final_words = words[0:num_words]
+    return final_words
+    
+def get_game_info(size=SIZE_TYPE_NORMAL):
+    ginfo = game_info.GameInfo()
+    ginfo.game_id = 0
+    ginfo.adv_color = game_info.RED
+    if size == SIZE_TYPE_NORMAL:
+        blue = 8
+        red = 8
+        bystanders = 7
+    else:
+        blue = 8
+        red = 8
+        num_blackouts = 9
+        bystanders = 81 - num_blackouts - blue - red - 1
+    if random.random() < 0.5:
+        blue += 1
+        ginfo.adv_color = game_info.BLUE
+    else:
+        red += 1
+    mix = ["B"] * blue
+    mix.extend(["R"] * red)
+    mix.extend(["X"])
+    if size == SIZE_TYPE_BIG:
+        mix.extend(['X'])
+    mix.extend(["O"] * bystanders)
+    random.shuffle(mix)
+    if size == SIZE_TYPE_BIG:
+        for i in BIG_BLACKOUT_SPOTS:
+            mix.insert(i, '-')
+    ginfo.layout = mix
+    return ginfo
+    
+def get_leader_html(seed=None, size=SIZE_TYPE_NORMAL):
+    if seed:
+        random.seed(seed)
+    html = ''
+    ginfo = get_game_info(size)
+    color_text = "Red"
+    if ginfo.adv_color == game_info.BLUE:
+        color_text = "Blue"
+    count = 0
+    html += '<h3 style="color: ' + get_color(ginfo.adv_color) + '">' + color_text + ' starts</h3>'
+    if size == SIZE_TYPE_NORMAL:
+        html += "<table style='width: 500px; height: 500px'>"
+        for r in range (0,5):
+            html += "<tr>"
+            for c in range (0,5):
+                html += "<td style=\"border: 1px solid black; padding: 20px; background: " + get_color(ginfo.layout[count]) + ";\">&nbsp;&nbsp;&nbsp;</td>"
+                count += 1
+            html += "</tr>"
+        html += "</table>"
+    elif size == SIZE_TYPE_BIG:
+        html += "<table style='width: 1000px; height: 1000px'>"
+        for r in range (0,9):
+            html += "<tr>"
+            for c in range (0,9):
+                html += "<td style=\"border: 1px solid black; padding: 20px; background: " + get_color(ginfo.layout[count]) + ";\">&nbsp;&nbsp;&nbsp;</td>"
+                
+                count += 1
+            html += "</tr>"
+        html += "</table>"
+    
+    return html
+
+
+
+def get_game_html(dictionary,seed=None, size=SIZE_TYPE_NORMAL):
+    if seed:
+        random.seed(seed)
+    html = ''
+    count = 0
+    html += "<table style='table-layout: fixed; width: 100%; height: 100%'>"
+    #html += "<table style='table-layout: fixed; width: 800px; height: 600px'>"
+    num_words = NUM_WORDS_NORMAL
+    if size == SIZE_TYPE_BIG:
+        num_words = NUM_WORDS_BIG
+    words = get_player_info(dictionary, num_words)
+    if size == SIZE_TYPE_NORMAL:
+        for r in range (0,5):
+            html += "<tr>"
+            for c in range (0,5):
+                html += "<td class='codename_cell' id='cell_"+str(r)+str(c)+"'style=\"padding: 5px; border: 1px solid black; background: " + white_color + "; font-size: 100%; text-align: center\">" + words[count] +"</td>"
+                count += 1
+            html += "</tr>"
+        html += "</table></br>"
+    elif size == SIZE_TYPE_BIG:
+        for r in range (0,9):
+            html += "<tr>"
+            for c in range (0,9):
+                if 9*r+c in BIG_BLACKOUT_SPOTS:
+                    html += "<td class='codename_cell' id='cell_"+str(r)+str(c)+"'style=\"padding: 5px; border: 1px solid black; background: " + blackout_color + "; font-size: 100%; text-align: center\"></td>"
+                else:
+                    html += "<td class='codename_cell' id='cell_"+str(r)+str(c)+"'style=\"padding: 5px; border: 1px solid black; background: " + white_color + "; font-size: 100%; text-align: center\">" + words[count] +"</td>"
+                count += 1
+            html += "</tr>"
+        html += "</table></br>"
+    return html
