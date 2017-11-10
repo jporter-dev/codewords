36f5e44e3ccd773d1b1dc980d491503ecf185f5b
Joshua Porter <joshporter1@gmail.com>
2017-11-09 21:06:09 -0500

    Hooked up game model to sockets.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room, leave_room, send, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send
 from flask_bootstrap import Bootstrap, StaticCDN
-from codename import codename_view
-import socket
+from codenames import game
 import random
 import string
-import json
-import logging
-import logging.handlers
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,58 +12,12 @@ app.extensions['bootstrap']['cdns']['jquery'] = StaticCDN()
 app.extensions['bootstrap']['cdns']['bootstrap'] = StaticCDN()
 app.secret_key = b'FF\x90}\xdc\xc5\xaeaT\xd6\xbc\x86O\xa6B\xdd\xa2qp\x9e\xd2f\xe8\xe8'
 
-LOG_FILENAME='/home/sprek/logs/user/flask_webapp.log'
-
-cur_host = socket.gethostname()
-if 'webfaction' in cur_host:
-    file_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=5242880, backupCount=10)
-    app.logger.addHandler(file_handler)
+rooms = {}
 
 # sprek's homepage
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-# --------------------------------------------------                                                                                                                                                                 
-# codename                                                                                                                                                                                                          
-
-# codenames landing page
-# choose player role [leader, player]
-@app.route('/codename')
-def codename():
-    return render_template('codename/codename.html')
-
-# leader view
-@app.route('/codename/leader')
-def leader():
-    return render_template('codename/leader.html', code_html=codename_view.get_leader_html())
-
-# REST endpoint to set leader seed
-@app.route('/codename/set_seed_leader', methods=('POST','GET'))
-def set_seed_leader():
-    seed=int(request.form["seed_input"].strip())
-    size=request.form["size_select"].strip()
-    players=request.form["players_select"].strip()
-    players=int(players)
-    return codename_view.get_leader_html(seed, size, players)
-
-# REST endpoint to set player seed
-@app.route('/codename/set_seed_game', methods=('POST','GET'))
-def set_seed_game():
-    seed = None
-    if request.form["seed_input"]:
-        seed=int(request.form["seed_input"].strip())
-    dict_select=request.form["dict_select"].strip()
-    size=request.form["size_select"].strip()
-    
-    return codename_view.get_game_html(dict_select,seed,size)
-
-# player view
-@app.route('/codename/play')
-def play():
-    return render_template('codename/play.html', code_html=codename_view.get_game_html("Dictionary", size='normal'))
-
 
 # BEGIN CODENAMESv2 -- WEBSOCKETS
 # required params
@@ -81,37 +31,44 @@ def play():
 #   # of teams - v2.1?
 @socketio.on('create')
 def on_create(data):
+    username = data['username']
+    # create the room
     id_length = 5
     room = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(id_length))
-    username = data['username']
     join_room(room)
-    send(username + ' has created the room. [' + room + ']')
-    emit('room_created', {"room": room})
+    # create the game
+    rooms[room] = game.Info(game_id=room)
+    rooms[room].add_player(username)
+    send(rooms[room].to_json(), room=room)
 
 # join a game
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['room']
+    # add player and rebroadcast game object
+    rooms[room].add_player(username)
     join_room(room)
-    send(username + ' has entered the room.', room=room)
+    send(rooms[room].to_json(), room=room)
 
 # leave a game
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['room']
+    # add player and rebroadcast game object
+    rooms[room].remove_player(username)
     leave_room(room)
-    send(username + ' has left the room.', room=room)
+    send(rooms[room].to_json(), room=room)
 
 # take a turn
-@socketio.on('turn')
-def on_turn(data):
-    print(data)
-    username = data['username']
+@socketio.on('flip_card')
+def on_flip_card(data):
+    # flip card and rebroadcast game object
     room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
+    card = data['card']
+    rooms[room].flip_card(card)
+    send(rooms[room].to_json(), room=room)
 
 # set spymaster
 
--- codenames/game.py
+++ codenames/game.py
@@ -1,5 +1,6 @@
 # object for tracking game status
 import random
+import json
 
 # dictionaries
 FILE_ROOT="/Users/jporter/code/codenames"
@@ -19,15 +20,41 @@ NUM_WORDS_BIG=81
 BIG_BLACKOUT_SPOTS=[4,20,24,36,40,44,56,60,76]
 
 class Info:
-    def __init__(self, dictionary='Dictionary', seed=None, size=NUM_WORDS_NORMAL, players=2):
-        self.adv_color = 'adv_color' #what is this
-        self.dictionary = dictionary
-        self.players = players
+    def __init__(self, game_id=1, dictionary='Dictionary', seed=None, size=NUM_WORDS_NORMAL, teams=2):
+        self.game_id = game_id
         self.size = size
-        self.layout = self.__get_layout()
+        self.starting_color = RED
+        self.teams = teams
+        self.players = []
+        self.dictionary = dictionary
+        self.dictionaries = DICTIONARIES.keys()
         self.words = self.__get_words()
+        self.layout = self.__get_layout()
+        self.board = dict.fromkeys(self.words, False)
         self.solution = dict(zip(self.words, self.layout))
 
+    def to_json(self):
+        return {
+            "game_id": self.game_id,
+            "starting_color": self.starting_color,
+            "players": self.players,
+            "words": self.words,
+            "board": self.board,
+            "layout": self.layout,
+            "solution": self.solution,
+        }
+
+    def flip_card(self, word):
+        if word not in self.words:
+            return 'Invalid word entered.'
+        self.board[word] = self.solution[word]
+        return self.solution[word]
+
+    def add_player(self, name):
+        self.players.append(name)
+        
+    def remove_player(self, name):
+        self.players.remove(name)
 
     def __get_words(self):
         if not self.dictionary in DICTIONARIES.keys():
@@ -40,10 +67,10 @@ class Info:
         return final_words
 
     def __get_layout(self):
-        self.adv_color = RED
+        self.starting_color = RED
         green = 0
         if self.size == NUM_WORDS_NORMAL:
-            if self.players==3:
+            if self.teams==3:
                 blue = 5
                 red = 5
                 green = 5
@@ -53,28 +80,28 @@ class Info:
 
             bystanders = 25 - blue - red - green - 2
         else:
-            if self.players == 2:
+            if self.teams == 2:
                 blue = 8
                 red = 8
-            elif self.players == 3:
+            elif self.teams == 3:
                 blue = 8
                 red = 8
                 green = 8
             num_blackouts = 9
             bystanders = 81 - num_blackouts - blue - red - green - 3
-        if self.players == 2:
+        if self.teams == 2:
             if random.random() < 0.5:
                 blue += 1
-                self.adv_color = BLUE
+                self.starting_color = BLUE
             else:
                 red += 1
         else:
             if random.random() < 0.333:
                 blue += 1
-                self.adv_color = BLUE
+                self.starting_color = BLUE
             elif random.random() > 0.666:
                 green += 1
-                self.adv_color = GREEN
+                self.starting_color = GREEN
             else:
                 red += 1
         mix = ["B"] * blue
