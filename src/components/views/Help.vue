ecaa11570fa985971599a40c2d8bb898da5fcd18
Joshua Porter <joshporter1@gmail.com>
2017-11-21 22:32:00 -0500

    Updated help text on Help.vue

--- src/components/views/Help.vue
+++ src/components/views/Help.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-fade-transition appear>  
+  <v-fade-transition appear>
     <v-layout row wrap pt-5>
       <v-flex>
         <v-card tile flat dark>
           <v-card-text px-0 class='text-xs-left'>
+            <h4>Getting Started</h4>
+            <p>
+              This application is designed to be played on multiple devices. The "agent"
+              view can be loaded on a TV or mobile devies, and the "spymaster" view
+              should be activated on the phones of the spymasters.
+            </p>
             <h4 class="header">How to Play</h4>
             <p>Codenames consists of two teams with two roles on each team: a spymaster and field agents.</p>
-            <h4 class="subheading">Spymasters</h4>
-            <h4 class="subheading">Field Agents</h4>
+            <p>
+              <a href="https://www.boardgamebriefs.com/codenames/">View detailed instructons.</a>
+            </p>
           </v-card-text>
         </v-card>
       </v-flex>
