89b81ea6c1b4682b175ff76e5b84fcb6c99f3c03
Joshua Porter <joshporter1@gmail.com>
2018-04-04 19:47:16 -0400

    Fix overscroll bounce in browser

--- src/App.vue
+++ src/App.vue
@@ -175,4 +175,13 @@ export default {
 </script>
 
 <style>
+/* prevent overscrolling bounce */
+html {
+    overflow: hidden;
+    height: 100%;
+}
+body {
+    height: 100%;
+    overflow: auto;
+}
 </style>
