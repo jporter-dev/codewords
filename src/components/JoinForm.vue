b37ae6f2e87ec3debf1b471bf1aba0212f9faa5f
Joshua Porter <joshporter1@gmail.com>
2018-04-05 19:07:18 -0400

    Add room ID check before submitting to suppress an error

--- src/components/JoinForm.vue
+++ src/components/JoinForm.vue
@@ -4,7 +4,10 @@
       <v-card-text>
         <v-form @submit="joinGame">
           <!-- <v-text-field label="Username" v-model="username" required></v-text-field> -->
-          <v-text-field label="Enter Room ID" v-model="room_num" solo light></v-text-field>
+          <v-alert type="error" :value="showInputError" transition="slide-y-reverse-transition">
+            Room ID required to join.
+          </v-alert>
+          <v-text-field placeholder="Enter Room ID" v-model="room_num" :rules="[rules.required]" solo light></v-text-field>
           <v-btn block color="primary" large @click.stop="joinGame">Join</v-btn>
         </v-form>
       </v-card-text>
@@ -13,32 +16,42 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'create-form',
+  name: "create-form",
   data() {
     return {
       // username: '',
       room_num: null,
+      showInputError: false,
+      rules: {
+        required: value => !!value || "Required."
+      }
     };
   },
   computed: {
     room_id() {
       return this.room_num.toUpperCase();
-    },
+    }
   },
   methods: {
-    ...mapMutations(['set_username', 'set_room']),
+    ...mapMutations(["set_username", "set_room"]),
     joinGame() {
       // this.set_username(this.username);
-      this.set_room(this.room_id);
-      this.$router.push({ name: 'Player', params: { room: this.room_id } });
-    },
-  },
+      this.showInputError = false;
+      if (this.room_num) {
+        this.set_room(this.room_id);
+        this.$router.push({ name: "Player", params: { room: this.room_id } });
+      } else {
+        this.showInputError = true;
+      }
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
