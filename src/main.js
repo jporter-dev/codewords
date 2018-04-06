d60b00988781423a13d80cfec11f4c8875c7b47e
Joshua Porter <joshporter1@gmail.com>
2018-04-06 11:42:42 -0400

    Added route persistence

--- src/main.js
+++ src/main.js
@@ -12,9 +12,6 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker'
 
-// vue router sync with vuex
-sync(store, router) // done. Returns an unsync callback fn
-
 // add bugsnag
 import * as bugsnag from 'bugsnag-js'
 import * as bugsnagVue from 'bugsnag-vue'
@@ -34,4 +31,13 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  beforeCreate () {
+    // before creating vue app, check if current path doesn't match stored path
+    // check if store contains a route first
+    if (this.$store.state.route && (this.$route.path !== this.$store.state.route.path)) {
+      this.$router.push(this.$store.state.route.path)
+    }
+    // vue router sync with vuex
+    sync(store, router) // done. Returns an unsync callback fn
+  },
 }).$mount('#app');
