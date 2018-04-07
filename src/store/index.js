c5034452a54bfc44d576cb5de76fe42c3e60ffa8
Joshua Porter <joshporter1@gmail.com>
2018-04-07 01:17:15 -0400

    Switched to using cookies for persisted state to allow TTL

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  plugins: [createPersistedState()],
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => Cookies.get(key),
+      // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+      setItem: (key, value) => Cookies.set(key, value, { expires: 1, secure: true }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })],
   state: {
     connected: false,
     dictionaries: [],
