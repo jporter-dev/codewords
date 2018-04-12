163e1654ff1b4c0866915fa96383ec413bba96b1
Joshua Porter <joshporter1@gmail.com>
2018-04-11 20:48:45 -0400

    Add DictionaryMixer component

--- src/components/CreateForm.vue
+++ src/components/CreateForm.vue
@@ -3,28 +3,43 @@
     <v-card>
       <v-card-text>
         <v-form @submit="createGame" v-model="valid" id="create-form">
-          <!-- <v-text-field label="Username" v-model="username" required></v-text-field> -->
-          <v-switch v-model="useCustom" label="Use a custom word bank"></v-switch>
-          <v-select v-bind:items="dictionaries" v-model="dictionary" label="Dictionary" required v-if="!useCustom"></v-select>
-          <v-text-field
-            name="custom-wordbank"
-            label="Custom Wordbank"
-            multi-line
-            v-model="rawWordbank"
-            :rules="wordbankRules"
-            placeholder="Enter a comma or newline separated list of words."
-            v-if="useCustom"
-          ></v-text-field>
-          <v-radio-group v-model="teams" row label="Teams">
-            <v-radio label="2 teams" value="2" ></v-radio>
-            <v-radio label="3 teams" value="3"></v-radio>
-          </v-radio-group>
-          <v-radio-group v-model="size" row label="Board Size">
-            <v-radio label="Normal" value="normal" ></v-radio>
-            <v-radio label="Large" value="large"></v-radio>
-          </v-radio-group>
-
-          <v-btn block color="primary" large @click.stop="createGame" :disabled="!valid" id="create-btn">Create</v-btn>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-radio-group v-model="dictionaryType" row label="Dictionary Selection">
+                <v-radio label="Default" value="single" ></v-radio>
+                <v-radio label="Custom" value="custom" ></v-radio>
+                <v-radio label="Mixer" value="mixer" ></v-radio>
+              </v-radio-group>
+            </v-flex>
+            <v-flex xs12>
+              <v-select v-bind:items="dictionaries" v-model="dictionary" label="Dictionary" required v-if="dictionaryType === 'single'"></v-select>
+              <v-text-field
+                name="custom-wordbank"
+                label="Custom Wordbank"
+                multi-line
+                v-model="rawWordbank"
+                :rules="wordbankRules"
+                placeholder="Enter a comma or newline separated list of words."
+                v-if="dictionaryType === 'custom'"
+              ></v-text-field>
+              <dictionary-mixer v-if="dictionaryType === 'mixer'"></dictionary-mixer>
+            </v-flex>
+            <v-flex xs6>
+              <v-radio-group v-model="teams" label="Teams">
+                <v-radio label="2 teams" value="2" ></v-radio>
+                <v-radio label="3 teams" value="3"></v-radio>
+              </v-radio-group>
+            </v-flex>
+            <v-flex xs6>
+              <v-radio-group v-model="size" label="Board Size">
+                <v-radio label="Normal" value="normal" ></v-radio>
+                <v-radio label="Large" value="large"></v-radio>
+              </v-radio-group>
+            </v-flex>
+            <v-flex xs12>
+              <v-btn block color="primary" large @click.stop="createGame" :disabled="!valid" id="create-btn">Create</v-btn>
+            </v-flex>
+          </v-layout>
         </v-form>
       </v-card-text>
     </v-card>
@@ -32,10 +47,12 @@
 </template>
 
 <script>
+import DictionaryMixer from '@/components/DictionaryMixer'
 import { mapState, mapMutations } from 'vuex';
 
 export default {
   name: 'create-form',
+  components: { DictionaryMixer },
   data() {
     return {
       // username: 'unknown',
@@ -43,7 +60,7 @@ export default {
       teams: '2',
       size: 'normal',
       valid: true,
-      useCustom: false,
+      dictionaryType: 'single',
       rawWordbank: '',
       wordbankRules: [
         () => (this.wordbank && this.wordbank.length >= 25) || 'Word bank must contain at least 25 words.',
@@ -71,7 +88,7 @@ export default {
         teams: this.teams,
         size: this.size,
       };
-      if (this.useCustom) {
+      if (this.dictionaryType === 'custom') {
         params.wordbank = this.wordbank;
       } else {
         params.dictionary = this.dictionary;
---
+++ src/components/DictionaryMixer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <v-switch
+        label="Mix Dictionaries"
+        v-model="mix"
+      ></v-switch>
+      <v-select
+        :items="dictionaries"
+        v-model="selectedDictionaries"
+        :multiple="mix"
+        max-height="400"
+        label="Select dictionaries to mix"
+        persistent-hint
+      ></v-select>
+    </v-flex>
+    <v-flex v-if="mix" xs12 v-for="dict in selectedDictionaries" :key="dict">
+      <v-slider v-model="mixes[dict]" :hint="dict" persistent-hint thumb-label step="10" ticks></v-slider>
+    </v-flex>
+    {{selectedDictionaries}}
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "DictionaryMixer",
+  data () {
+    return {
+      mix: false,
+      dictionaries: ['Standard', 'CAH', 'German', 'French'],
+      selectedDictionaries: [],
+      mixes: {},
+    }
+  },
+}
+</script>
