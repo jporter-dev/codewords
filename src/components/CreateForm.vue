8edf16bca3238ba401a36b498134a757d6e77f26
Joshua Porter <joshporter1@gmail.com>
2018-04-12 00:08:31 -0400

    Updated dictionary selection

--- src/components/CreateForm.vue
+++ src/components/CreateForm.vue
@@ -5,24 +5,7 @@
         <v-form @submit="createGame" v-model="valid" id="create-form">
           <v-layout row wrap>
             <v-flex xs12>
-              <v-radio-group v-model="dictionaryType" row label="Dictionary Selection">
-                <v-radio label="Default" value="single" ></v-radio>
-                <v-radio label="Custom" value="custom" ></v-radio>
-                <v-radio label="Mixer" value="mixer" ></v-radio>
-              </v-radio-group>
-            </v-flex>
-            <v-flex xs12>
-              <v-select v-bind:items="dictionaries" v-model="dictionary" label="Dictionary" required v-if="dictionaryType === 'single'"></v-select>
-              <v-text-field
-                name="custom-wordbank"
-                label="Custom Wordbank"
-                multi-line
-                v-model="rawWordbank"
-                :rules="wordbankRules"
-                placeholder="Enter a comma or newline separated list of words."
-                v-if="dictionaryType === 'custom'"
-              ></v-text-field>
-              <dictionary-mixer v-if="dictionaryType === 'mixer'"></dictionary-mixer>
+              <dictionary-mixer @setDictionaryOptions="setDictionaryOptions"></dictionary-mixer>
             </v-flex>
             <v-flex xs6>
               <v-radio-group v-model="teams" label="Teams">
@@ -55,24 +38,14 @@ export default {
   components: { DictionaryMixer },
   data() {
     return {
-      // username: 'unknown',
-      dictionary: 'Simple',
       teams: '2',
       size: 'normal',
       valid: true,
-      dictionaryType: 'single',
-      rawWordbank: '',
-      wordbankRules: [
-        () => (this.wordbank && this.wordbank.length >= 25) || 'Word bank must contain at least 25 words.',
-      ],
+      dictionaryOptions: {}
     };
   },
   computed: {
     ...mapState(['room', 'dictionaries']),
-    wordbank() {
-      // return wordbank split on commas and newlines and uniqued
-      return [...new Set(this.rawWordbank.split(/[\n,]/))].filter(String);
-    },
   },
   watch: {
     room() {
@@ -84,22 +57,16 @@ export default {
     ...mapMutations(['set_username', 'set_room']),
     createGame() {
       const params = {
-        // username: this.username,
         teams: this.teams,
         size: this.size,
+        dictionaryOptions: this.dictionaryOptions
       };
-      if (this.dictionaryType === 'custom') {
-        params.wordbank = this.wordbank;
-      } else {
-        params.dictionary = this.dictionary;
-      }
       this.set_username(this.username);
       this.$socket.emit('create', params);
     },
+    setDictionaryOptions (opts) {
+      this.dictionaryOptions = opts
+    }
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
--- src/components/DictionaryMixer.vue
+++ src/components/DictionaryMixer.vue
@@ -2,19 +2,39 @@
   <v-layout row wrap>
     <v-flex xs12>
       <v-switch
+        label="Use a custom wordbank"
+        v-model="useCustom"
+      ></v-switch>
+      <v-switch
+        v-show="!useCustom"
         label="Mix Dictionaries"
         v-model="mix"
       ></v-switch>
+      <v-text-field
+        v-show="useCustom"
+        name="custom-wordbank"
+        label="Custom Wordbank"
+        multi-line
+        v-model="rawWordbank"
+        :rules="wordbankRules"
+        placeholder="Enter a comma or newline separated list of words."
+      ></v-text-field>
       <v-select
-        :items="dictionaries"
+        v-show="!useCustom"
         v-model="selectedDictionaries"
+        :items="dictionaries"
         :multiple="mix"
         max-height="400"
-        label="Select dictionaries to mix"
+        label="Dictionary"
+        placeholder="Select..."
         persistent-hint
+        @change="setMixes"
       ></v-select>
     </v-flex>
-    <v-flex v-if="mix && typeof selectedDictionaries === 'object'" xs12 v-for="dict in selectedDictionaries" :key="dict">
+    <v-flex
+      v-show="mix && typeof selectedDictionaries === 'object' && selectedDictionaries && selectedDictionaries.length > 1"
+      xs12
+      v-for="(val, dict) in mixes" :key="dict">
       <v-slider v-model="mixes[dict]"
         :hint="dict"
         step="5"
@@ -31,25 +51,59 @@ export default {
   name: "DictionaryMixer",
   data () {
     return {
-      mix: false,
+      mix: true,
       dictionaries: ['Standard', 'CAH', 'German', 'French'],
       selectedDictionaries: [],
       mixes: {},
+      useCustom: false,
+      rawWordbank: '',
+      wordbankRules: [
+        () => (this.wordbank && this.wordbank.length >= 25) || 'Word bank must contain at least 25 words.',
+      ],
+    }
+  },
+  watch: {
+    dictionaryOptions: {
+      handler: function () { this.$emit('setDictionaryOptions', this.dictionaryOptions) },
+      immediate: true
     }
   },
   computed: {
+    dictionaryOptions () {
+      return {
+        mix: this.mix,
+        useCustom: this.useCustom,
+        selectedDictionaries: this.selectedDictionaries,
+        mixPercentages: this.mixPercentages,
+        customWordbank: this.wordbank
+      }
+    },
+    // get total of dict sliders
     mixTotal () {
-      return (this.mix && this.selectedDictionaries.length > 0) ?
+      return (this.mix && this.selectedDictionaries && this.selectedDictionaries.length > 0) ?
         Object.values(this.mixes)
           .map((val) => val || 0) // default values to 0
           .reduce((a, b) => { return a+b }, 0) : 0 // sum values
     },
+    // map dicts to % of total
     mixPercentages () {
       let that = this
       return Object.keys(that.mixes).reduce(function(obj, key) {
         obj[key] = (Math.floor((that.mixes[key] * 100) / that.mixTotal) || 0).toString() + "%"
         return obj;
       }, {});
+    },
+    wordbank() {
+      // return wordbank split on commas and newlines and uniqued
+      return [...new Set(this.rawWordbank.split(/[\n,]/))].filter(String);
+    },
+  },
+  methods: {
+    // initialize dict mixes
+    setMixes (dicts) {
+      dicts.forEach((dict) => {
+        this.$set(this.mixes, dict, 50)
+      })
     }
   }
 }
