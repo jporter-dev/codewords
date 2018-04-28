fad30f3b8907133974fe4bd40ff95c7692354adb
Joshua Porter <joshporter1@gmail.com>
2018-04-27 20:47:08 -0400

    Began writing kitchen sink e2e tests

--- tests/e2e/specs/app.js
+++ tests/e2e/specs/app.js
@@ -1,16 +1,24 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Codenames', () => {
+describe('Codenames App', () => {
   it('Loads the homepage', () => {
     cy.visit('/')
     cy.hash().should('eq', '#/home')
-    cy.get('#create-btn').should('contain', 'Create')
+
   })
-  it('Loads the create form', () => {
-    cy.visit('/#/create')
-    cy.hash().should('eq', '#/create')
-    cy.get('#create-btn').should('contain', 'Create')
+  it('Clicks the create button and displays the loading form', () => {
+    cy.get('#create-btn').should('contain', 'Create').click()
+    cy.contains('Use a custom wordbank')
+    cy.contains('Mix Dictionaries')
+    // cy.get('#create-btn').should('contain', 'Create').click()
   })
+
+  // it('Creates a game and loads the Player view', () => {
+  //   cy.get('#shuffle-btn').should('contain', 'Shuffle Words')
+  // })
+
+  // it('Switches to the Spymaster view', () => {})
+
   it('Loads the player view', () => {
     cy.visit('/#/12345/player')
     cy.hash().should('eq', '#/12345/player')
@@ -20,5 +28,11 @@ describe('Codenames', () => {
     cy.visit('/#/12345/spymaster')
     cy.hash().should('eq', '#/12345/spymaster')
     cy.get('#spymaster-btn').should('contain', 'I understand. Make me a spymaster!')
+  })
+
+  it('Visits the homepage', () => {
+    cy.visit('/')
+    cy.hash().should('eq', '#/home')
+    cy.get('#create-btn').should('contain', 'Create')
   })
 })
