5df1f8f0279b00848604b8f20c1e6f8cfb891815
Joshua Porter <joshporter1@gmail.com>
2018-07-18 00:03:57 -0400

    Update Raivue and .travis.yml

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -12,10 +12,10 @@
             <v-flex sm2 xs12>
               <v-btn block round large color="primary" href="https://www.paypal.me/joshporter1">PayPal</v-btn>
             </v-flex>
-            <RaiDonation
+            <NanoPayment
               address="xrb_3ui4sg4jjdxsfwshjcbkjnthdzmtbgxe7pa597cxsa4aamkkj3b8dmeome4i"
-              label="Donate"
-            ></RaiDonation>
+              :amount="2500000"
+            ></NanoPayment>
           </center>
           <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         </v-card-text>
