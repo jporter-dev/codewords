d69070fcb6dd66a77c253d921fe894f7e50b061e
Josh Porter <joshporter1@gmail.com>
2019-09-11 00:12:19 -0400

    Minor bug fixes

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -2,14 +2,14 @@
   <v-container fluid grid-list-md fill-height pa-0 v-if="role">
     <v-layout column>
       <slot></slot>
-      <v-flex xs12 pa-0 v-for="row in gridRows" :key="row">
-        <v-layout row wrap fill-height>
+      <v-flex pa-0 v-for="row in gridRows" :key="row">
+        <v-layout row wrap :fill-height="$vuetify.breakpoint.mdAndUp">
           <v-flex d-flex class="cn-card" v-for="cell in gridCells" @click="showFlipCard(getWord(row, cell))" :key="cell">
             <v-fade-transition appear>
               <v-card flat tile :color="getColor(getWord(row, cell), getTeam(getWord(row, cell)))" class="text-xs-center">
                 <v-layout fill-height align-center ma-0>
                   <v-flex pa-0>
-                    <v-card-text class="cn-text cn-text--upcase" v-resize-text="{ratio: 0.9, delay: 500}">
+                    <v-card-text class="cn-text cn-text--upcase" v-resize-text="{ratio: 0.9, delay: 200}">
                       {{getWord(row, cell)}}
                     </v-card-text>
                   </v-flex>
