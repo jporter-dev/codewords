8d89a4c6261014def140a3d879d38e162b95b51f
Josh Porter <joshporter1@gmail.com>
2020-04-14 15:13:46 -0400

    Update font size for cards

--- frontend/src/components/game/Card.vue
+++ frontend/src/components/game/Card.vue
@@ -12,7 +12,7 @@
       <div class="d-flex flex-column fill-height">
         <div
           class="d-flex flex-grow-1 align-center justify-center text-center cn-word py-3 cn-text cn-text--upcase"
-          v-resize-text="{ratio: 1.1}"
+          v-resize-text="{ratio: 1.1, minFontSize, maxFontSize}"
         >
           {{word}}
         </div>
@@ -36,6 +36,14 @@
 <script>
 export default {
   props: ["word", "team", "colors"],
+  computed: {
+    minFontSize () {
+      return this.$vuetify.breakpoint.mdAndUp ? 20 : 16
+    },
+    maxFontSize () {
+      return this.$vuetify.breakpoint.mdAndUp ? 30 : 24
+    }
+  },
   methods: {
     getTeamName(team) {
       switch (team) {
