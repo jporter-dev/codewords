85b9cabe74c3acab581af447d43b33fa6137b79d
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-07-06 16:24:26 -0500

    Fixing runtime errors

--- frontend/src/components/game/Card.vue
+++ frontend/src/components/game/Card.vue
@@ -1,22 +1,12 @@
 <template>
-  <v-card
-    :color="colors.background"
-    class="fill-height cn-card telegram"
-    raised
-    tile
-  >
+  <v-card :color="colors.background" class="fill-height cn-card telegram" raised tile>
     <slot name="title"></slot>
-    <v-card-text
-      class="fill-height pa-1"
-      :class="colors.font"
-    >
+    <v-card-text class="fill-height pa-1" :class="colors.font">
       <div class="d-flex flex-column fill-height">
         <div
           class="d-flex flex-grow-1 align-center justify-center text-center cn-word py-3 cn-text cn-text--upcase"
           v-resize-text="{ratio: 1.1, minFontSize, maxFontSize}"
-        >
-          {{word}}
-        </div>
+        >{{word}}</div>
         <div
           class="d-flex flex-shrink-1 align-end justify-end cn-team"
           v-if="$vuetify.breakpoint.mdAndUp"
@@ -24,9 +14,7 @@
           <h6
             class="cn-text cn-text--upcase"
             v-resize-text="{maxFontSize: 16, minFontSize: 12, ratio: 2}"
-          >
-            {{getTeamName(team)}}
-          </h6>
+          >{{getTeamName(team)}}</h6>
         </div>
       </div>
     </v-card-text>
@@ -49,15 +37,15 @@ export default {
     getTeamName(team) {
       switch (team) {
         case "R":
-          return $('red');
+          return `$t("red")`;
         case "B":
-          return $t('blue');
+          return `$t("blue")`;
         case "G":
-          return $t('green');
+          return `$t("green")`;
         case "O":
-          return $t('citizen');
+          return `$t("citizen")`;
         case "X":
-          return $('assassin');
+          return `$t("assassin")`;
         default:
           return "";
       }
