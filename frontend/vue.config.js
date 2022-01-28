a9a117a21a43b34a3b1c8d76d76b913f3cd033e2
Josh Porter <j@prtr.dev>
2022-01-27 21:09:06 -0500

    fix: container name references

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
     proxy: {
       '/socket.io': {
-        target: `http://${process.env.VUE_APP_FLASK_HOST}:3000`,
+        target: `http://${process.env.VUE_APP_FLASK_HOST}:5000`,
         ws: true,
         changeOrigin: true
       },
